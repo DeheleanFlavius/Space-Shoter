d09dc6415ec80a57c235ddef88a864dfd08e31d1
JARVIS <JARVIS@DESKTOP-AO88A59>
2023-05-04 08:22:33 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿using System.Reflection.Emit;
+using System.Threading;
+
+namespace Space_Shoter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.bullet = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.enemyThree = new System.Windows.Forms.PictureBox();
+            this.enemyTwo = new System.Windows.Forms.PictureBox();
+            this.enemyOne = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.BackColor = System.Drawing.Color.Transparent;
+            this.txtScore.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtScore.ForeColor = System.Drawing.Color.Red;
+            this.txtScore.Location = new System.Drawing.Point(0, 0);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(800, 700);
+            this.txtScore.TabIndex = 0;
+            this.txtScore.Text = "0";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.mainGameTimerEvent);
+            // 
+            // bullet
+            // 
+            this.bullet.BackColor = System.Drawing.Color.Transparent;
+            this.bullet.Image = global::Space_Shoter.Properties.Resources.munition;
+            this.bullet.Location = new System.Drawing.Point(420, 268);
+            this.bullet.Name = "bullet";
+            this.bullet.Size = new System.Drawing.Size(30, 30);
+            this.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bullet.TabIndex = 0;
+            this.bullet.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Image = global::Space_Shoter.Properties.Resources.player;
+            this.player.Location = new System.Drawing.Point(304, 559);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(90, 90);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // enemyThree
+            // 
+            this.enemyThree.BackColor = System.Drawing.Color.Transparent;
+            this.enemyThree.Image = global::Space_Shoter.Properties.Resources.enemyThree;
+            this.enemyThree.Location = new System.Drawing.Point(564, 82);
+            this.enemyThree.Name = "enemyThree";
+            this.enemyThree.Size = new System.Drawing.Size(90, 90);
+            this.enemyThree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.enemyThree.TabIndex = 0;
+            this.enemyThree.TabStop = false;
+            // 
+            // enemyTwo
+            // 
+            this.enemyTwo.BackColor = System.Drawing.Color.Transparent;
+            this.enemyTwo.Image = global::Space_Shoter.Properties.Resources.enemyTwo;
+            this.enemyTwo.Location = new System.Drawing.Point(304, 82);
+            this.enemyTwo.Name = "enemyTwo";
+            this.enemyTwo.Size = new System.Drawing.Size(90, 90);
+            this.enemyTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.enemyTwo.TabIndex = 0;
+            this.enemyTwo.TabStop = false;
+            // 
+            // enemyOne
+            // 
+            this.enemyOne.BackColor = System.Drawing.Color.Transparent;
+            this.enemyOne.Image = global::Space_Shoter.Properties.Resources.enemy;
+            this.enemyOne.Location = new System.Drawing.Point(87, 82);
+            this.enemyOne.Name = "enemyOne";
+            this.enemyOne.Size = new System.Drawing.Size(90, 90);
+            this.enemyOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.enemyOne.TabIndex = 0;
+            this.enemyOne.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(784, 661);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.enemyThree);
+            this.Controls.Add(this.enemyTwo);
+            this.Controls.Add(this.enemyOne);
+            this.Controls.Add(this.bullet);
+            this.Controls.Add(this.txtScore);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(800, 700);
+            this.MinimumSize = new System.Drawing.Size(800, 700);
+            this.Name = "Form1";
+            this.Text = "Space Force";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox enemyOne;
+        private System.Windows.Forms.PictureBox enemyTwo;
+        private System.Windows.Forms.PictureBox enemyThree;
+        private System.Windows.Forms.PictureBox bullet;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using WMPLib;
+
+namespace Space_Shoter
+{
+    public partial class Form1 : Form
+    {
+        WMPLib.WindowsMediaPlayer Shoot = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer explotion = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer gameOverSound = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer monsterKill = new WMPLib.WindowsMediaPlayer();
+        WMPLib.WindowsMediaPlayer rampage = new WMPLib.WindowsMediaPlayer();
+
+
+
+
+        bool goLeft, goRight, goUp, goDown, shooting, isGameOver;
+        int score;
+        int playerSpeed = 9;
+        int enemySpeed;
+        int bulletSpeed = 25;
+        Random rnd = new Random();
+        private System.Windows.Forms.PictureBox enemyBullet;
+        bool enemyShooting = false;
+        public Form1()
+        {
+            InitializeComponent();
+            initGameExtras();
+            resetGame();
+        }
+
+        public void initGameExtras()
+        {
+            this.enemyBullet = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyBullet)).BeginInit();
+            this.enemyBullet.Image = global::Space_Shoter.Properties.Resources.bullet;
+            this.enemyBullet.BackColor = System.Drawing.Color.Transparent;
+            this.enemyBullet.Location = new System.Drawing.Point(350, 321);
+            this.enemyBullet.Name = "bullet";
+            this.enemyBullet.Size = new System.Drawing.Size(7, 27);
+            this.enemyBullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.enemyBullet.TabIndex = 0;
+            this.enemyBullet.TabStop = false;
+            this.enemyBullet.Visible = true;
+            this.Controls.Add(this.enemyBullet);
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
+            this.enemyBullet.BringToFront();
+        }
+
+        private void mainGameTimerEvent(object sender, EventArgs e)
+        {
+
+            txtScore.Text = score.ToString();
+
+            enemyOne.Top += enemySpeed;
+            enemyTwo.Top += enemySpeed;
+            enemyThree.Top += enemySpeed;
+
+
+            if(enemyOne.Top > 650 || enemyTwo.Top > 650 || enemyThree.Top >650)
+            {
+                gameOver();
+            }
+
+            if (!enemyShooting)
+            {
+                Random random = new Random();
+                if (random.Next(1, 100) <= 5)
+                {
+                    enemyShooting = true;
+                    enemyBullet.Visible = true;
+                    var en = random.Next(0, 2);
+                    PictureBox[] list = { enemyOne, enemyTwo, enemyThree };
+                    if ((list[en].Bottom > 0) && (list[en].Bottom < this.Height / 2))
+                    {
+                        enemyBullet.Top = list[en].Bottom + 30;
+                        enemyBullet.Left = list[en].Left + list[en].Width / 2;
+                    } else
+                    {
+                        enemyShooting = false;
+                    }
+
+                    /*switch (en)
+                    {
+                        case 1:
+                            enemyBullet.Top = enemyOne.Bottom + 30;
+                            enemyBullet.Left = enemyOne.Left + enemyOne.Width / 2;
+                            break;
+                        case 2:
+                            enemyBullet.Top = enemyTwo.Bottom + 30;
+                            enemyBullet.Left = enemyTwo.Left + enemyThree.Width / 2;
+                            break;
+                        case 3:
+                            enemyBullet.Top = enemyThree.Bottom + 30;
+                            enemyBullet.Left = enemyThree.Left + enemyThree.Width / 2;
+                            break;
+                    }*/
+
+                }
+
+            }
+
+            if (enemyShooting)
+            {
+                enemyBullet.Top += bulletSpeed / 2;
+                if (enemyBullet.Bounds.IntersectsWith(player.Bounds))//when a object bounds with another
+                {
+                    explotion.URL = @"C:\Users\JARVIS\Desktop\Licenta\Space Shoter\sounds\boom.mp3";
+
+                    gameOver();
+                }
+                if (enemyBullet.Top > this.Bottom)
+                {
+                    enemyShooting = false;
+                    enemyBullet.Left = -300;
+                    enemyBullet.Visible = false;
+                }
+            }
+
+            //player movment logic starts
+
+            if(goLeft == true && player.Left > 0)
+            {
+                player.Left -= playerSpeed;
+            }
+            if(goRight == true && player.Left < 700)
+            {
+                player.Left += playerSpeed;
+            }
+            if(goUp == true && player.Top > 0) 
+            { 
+                player.Top -= playerSpeed; 
+            }
+            if (goDown == true && player.Top < 560)
+            {
+                player.Top += playerSpeed;
+            }
+
+            //player movment logic ends
+            //bullet logic starts
+            if (shooting == true)
+            {
+                //bulletSpeed = 25;
+                bullet.Top -= bulletSpeed;
+            }
+            else
+            {
+                bullet.Left = -300;
+                //bulletSpeed = 0;
+            }
+            if(bullet.Top < -30)
+            {
+                shooting = false;
+            }
+
+            if (bullet.Bounds.IntersectsWith(enemyOne.Bounds))//when a object bounds with another
+            {
+                explotion.URL = @"C:\Users\JARVIS\Desktop\Licenta\Space Shoter\sounds\boom.mp3";
+                score += 1;//score will increas by 1
+                enemyOne.Top = -450;//we reset the enemy to come again
+                enemyOne.Left = rnd.Next(20, 600);//we give him a random so he dosnt spown the same
+                shooting = false;
+            }
+            if (bullet.Bounds.IntersectsWith(enemyTwo.Bounds))
+            {
+                explotion.URL = @"C:\Users\JARVIS\Desktop\Licenta\Space Shoter\sounds\boom.mp3";
+                score += 1;
+                enemyTwo.Top = -700; //this one spowns more behaind
+                enemyTwo.Left = rnd.Next(20, 600);
+                shooting = false;
+            }
+            if (bullet.Bounds.IntersectsWith(enemyThree.Bounds))
+            {
+                explotion.URL = @"C:\Users\JARVIS\Desktop\Licenta\Space Shoter\sounds\boom.mp3";
+                score += 1;
+                enemyThree.Top = -800;
+                enemyThree.Left = rnd.Next(20, 600);
+                shooting = false;
+            }
+            //Game SPEED UP
+            if (score == 5)
+            {
+                monsterKill.URL = @"C:\Users\JARVIS\Desktop\Licenta\Space Shoter\sounds\Monster Kill.mp3";
+                playerSpeed = 12;
+                enemySpeed = 6;
+                txtScore.Text += Environment.NewLine + "MONSTER KILL!!!";
+            }
+            if(score == 10)
+            {
+                rampage.URL = @"C:\Users\JARVIS\Desktop\Licenta\Space Shoter\sounds\Rampage.mp3";
+                playerSpeed = 15;
+                enemySpeed = 8;
+                txtScore.Text += Environment.NewLine + "RAMPAGE!!!";
+            }
+            if (score >= 15 )
+            {
+                txtScore.Text = score.ToString();
+                youWin();
+                this.txtScore.BringToFront();
+            }
+
+
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if(e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+        }
+        //what happens when you reles a key
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if (e.KeyCode == Keys.Space && shooting == false)//shooting logic
+            {
+                shooting = true;
+                Shoot.URL = @"C:\Users\JARVIS\Desktop\Licenta\Space Shoter\sounds\shoot.mp3";
+
+                bullet.Top = player.Top + 30;//bullet spowns from spaceShip
+                bullet.Left = player.Left + (player.Width /2 ) -(bullet.Width /2);
+
+            }
+            if(e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                resetGame();
+                this.txtScore.SendToBack();
+            }
+        }
+
+        private void resetGame()
+        {
+            gameTimer.Start();
+            enemySpeed = 3;
+
+            enemyOne.Left = rnd.Next(20, 600);
+            enemyTwo.Left = rnd.Next(20, 600);
+            enemyThree.Left = rnd.Next(20, 600);
+
+            enemyOne.Top = rnd.Next(0, 200) * -1;//gives a spown point out of the form
+            enemyTwo.Top = rnd.Next(0, 500) * -1;
+            enemyThree.Top = rnd.Next(0,900) * -1;
+
+            score = 0;
+            bulletSpeed = 25;
+            bullet.Left = -300;
+            enemyBullet.Left = -300;
+            shooting = false;
+
+            txtScore.Text = score.ToString();
+
+        }
+
+        private void gameOver()
+        {
+            isGameOver = true;
+            gameOverSound.URL = @"C:\Users\JARVIS\Desktop\Licenta\Space Shoter\sounds\Game Over.mp3";
+
+            gameTimer.Stop();
+            txtScore.Text += Environment.NewLine + "Game Over!!!" + Environment.NewLine + "Press Enter to try again.";
+            if (isGameOver)
+            {
+                this.txtScore.BringToFront();
+            }
+
+        }
+
+        private void youWin()
+        {
+            isGameOver = true;
+            gameTimer.Stop();
+            txtScore.Text += Environment.NewLine + "you WON!!!" + Environment.NewLine + "Press Enter to Restart.";
+        }
+    }
+}
